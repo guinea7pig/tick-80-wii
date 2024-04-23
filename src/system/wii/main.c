659fa4dd1a75fdebde4668970f8af072b96baaca
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-04-23 08:38:44 -0400

    edited keyboard code

--- src/system/wii/main.c
+++ src/system/wii/main.c
@@ -3,26 +3,44 @@
 #include <stdlib.h>
 #include <wiilight.h>
 #include <kbd.h>
+#include "keyboard.h"
 //light int 
+int kbdfd = kbdinit();
+x=1
 WIILIGHT_Init();
 WIILIGHT_SetLevel(255):
 //keyboard studd
-void key_init()
 //code that hanndels stuff for virtaul and physicel
-char* kbdscan(int kbdfd);// scans for iputs figure out how to make this run on a loop
+
+
 void key_init()
 {
     int kbdfd = kbdinit();    
 }
+void key-to-tic_Key()
+{
+  int kbdfd = kbdinit();
+  char chars[29] = kbdscan(kbdfd);
+  char* pressed = kbdhandle(kbdfd, chars, false);
+  kbddenit(kbdfd);
+  return *pressed;
+   // code etecting if button is pressed
+
+}
 
 void keytest()
 {
-
+int kbdfd = kbdinit();
+char receivedmessage[15] = kbdscan(kbdfd);
+char* pressed = kbdhandle(kbdfd, receivedmessage, false);
+printf("%s was pressed\n", pressed);
+kbddeinit(kbdfd);
 }
 
 void kbv()//gets version of library for credit reasons
 {
 kbdver()
+printf("by Blooper4912")
 }
 //if input Letter: tic_name
 
@@ -35,4 +53,10 @@ WIILIGHT_TurnOn();
 sleep(1):
 WIILIGHT_TurnOff(); 
 }
-//if loading == true // do light stuff 
+//if loading == true // do light stuff extern int getKeyboardInput() {
+
+  int kbdfd = kbdinit();
+  char chars[15] = kbdscan(kbdfd);
+  char* pressed = kbdhandle(kbdfd, chars, false);
+  kbddenit(kbdfd);
+  return *pressed;
